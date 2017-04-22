596ebed09715fb0e9763a49ce6893c6464647fca
Praveen Seeniraj <praveens1997@gmail.com>
2017-04-22 16:55:27 -0400

    created a graph from the input data

--- river.c
+++ river.c
@@ -11,6 +11,9 @@ int leastplankRotations(char* filename)
 {
     int numberofRows;
     int numberofColumns;
+    int i;
+    int j;
+    int counter = 0;
     FILE* readptr = fopen(filename, "r");
     if(readptr == NULL)
     {
@@ -22,8 +25,32 @@ int leastplankRotations(char* filename)
     
     //printf("NumberofRows: %d NumberofColumn: %d\n", numberofRows, numberofColumns);
 
-    int** plankData = openFile(readptr, numberofRows, numberofColumns);
-
+    int** plankData = openFile(readptr, numberofRows, numberofColumns); // LAODING THE INPUT FILE INTO A MARTIX
+    // --------------------------------------- TEST --------------------------------------------------------------
+    /*for(i = 0; i < numberofRows; i++)
+    {
+        for(j = 0; j < numberofColumns; j++)
+        {
+            printf("%d ",plankData[i][j]);
+        }
+        printf("\n");
+    }*/
+    // ------------------------------------------------------------------------------------------------------------
+    struct Graph* plankGraph = createGraph(numberofRows*numberofColumns); // MAKING A GRAPH OUT OF THE INPUT MATRIX
+    // CREATING GRAPH EDGES
+    for(i = 0; i < numberofRows; i++)
+    {
+        for(j = 0; j < numberofColumns; j++)
+        {
+            if(plankData[i][j] == 1)
+            {
+                insertEdge(plankGraph,counter,counter+numberofColumns);
+            }
+            counter++;
+        }
+    }
+    //printGraph(plankGraph);
+    freeGraph(plankGraph);
     freeData(plankData, numberofRows, numberofColumns);
     fclose(readptr);
     return 0;
@@ -86,3 +113,79 @@ void freeData(int** plankData,int numberofRows, int numberofColumns)
     }
     free(plankData);
 }
+
+// Creating a new Adjecent List node
+AdjListNode* newAdjListNode(int dest)
+{
+    AdjListNode* newNode = malloc(sizeof(AdjListNode));
+    newNode->adjecent = dest;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Creating a new Graph
+Graph* createGraph(int V)
+{
+    Graph* newGraph = malloc(sizeof(Graph));
+    newGraph->vertex = V;
+
+    newGraph->array = malloc(V * sizeof(AdjList));
+    
+    // Initializing each array element as NULL in the graph
+    int AdjListCounter = 0;
+    for(AdjListCounter = 0; AdjListCounter < V; AdjListCounter++)
+    {
+        newGraph->array[AdjListCounter].head = NULL;
+    }
+    return newGraph;
+}
+
+void insertEdge(Graph* graph, int src, int dest)
+{
+    // Add an edge that points from source to destination or <u,v>
+    AdjListNode* newNode = newAdjListNode(dest);
+    newNode->next = graph->array[src].head;
+    graph->array[src].head = newNode;
+
+    // UNCOMMENT IF YOU WANT THE GRAPH TO BE UNDIRECTIONAL
+    newNode = newAdjListNode(src);
+    newNode->next = graph->array[dest].head;
+    graph->array[dest].head = newNode;
+}
+
+
+void freeGraph(Graph* freeGraph)
+{
+    int vertex = 0;
+    for(vertex = 0; vertex < freeGraph->vertex; vertex++)
+    {
+        while(freeGraph->array[vertex].head != NULL)
+        {
+            AdjListNode* temp = freeGraph->array[vertex].head;
+            freeGraph->array[vertex].head = temp->next;
+            free(temp);
+        }
+    }
+    free(freeGraph->array);
+    free(freeGraph);
+
+}
+
+// Printing the graph. CREATED FOR TESTING PURPOSES
+//-------------------- TEST-------------------------
+void printGraph(Graph* graph)
+{
+    int v;
+    for (v = 0; v < graph->vertex; ++v)
+    {
+        struct AdjListNode* pCrawl = graph->array[v].head;
+        printf("\n Adjacency list of vertex %d\n head ", v);
+        while (pCrawl)
+        {
+            printf("-> %d", pCrawl->adjecent);
+            pCrawl = pCrawl->next;
+        }
+        printf("\n");
+    }
+}
+//-----------------------TEST------------------------
--- river.h
+++ river.h
@@ -1,6 +1,27 @@
 #ifndef RIVER_H
 #define RIVER_H
+typedef struct AdjListNode
+{
+    int adjecent;
+    struct AdjListNode* next;
+}AdjListNode;
 
+typedef struct AdjList
+{
+    struct AdjListNode* head; 
+}AdjList;
+
+typedef struct Graph
+{
+    int vertex;
+    struct AdjList* array;
+}Graph;
+
+AdjListNode* newAdjListNode(int dest);
+Graph* createGraph(int V);
+void insertEdge(Graph* graph, int src, int dest);
+void printGraph(Graph* graph);
+void freeGraph(Graph* freeGraph);
 int leastplankRotations(char *filename);
 
 #endif
